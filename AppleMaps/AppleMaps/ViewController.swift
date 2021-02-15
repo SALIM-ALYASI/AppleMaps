3683e7443aae9aa1d83fcacc8476ee1c40b76734
walloffame002 <walloffame002@gmail.com>
2021-02-15 12:54:30 +0400

    1

---
+++ AppleMaps/AppleMaps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleMaps
+//
+//  Created by ALYASI on 15/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
